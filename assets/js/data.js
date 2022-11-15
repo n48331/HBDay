d2b2543b23d9b56fa1dc15aff6e1802cb37eb0f5
n48331 <unfolks@gmail.com>
2022-11-16 02:19:29 +0530

    Code simplified

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,122 +1,102 @@
 var today = new Date();
 
-var dd = parseInt(String(today.getDate()).padStart(2, '0'));
-var mm = parseInt(String(today.getMonth()).padStart(2, '0'));
+var dd = parseInt(String(today.getDate()).padStart(2, "0"));
+var mm = parseInt(String(today.getMonth()).padStart(2, "0"));
 
-var over = '<i class="uil uil-check"></i>'
-var notOver = '<i class="uil uil-arrow-up"></i>'
-var bday = '<i class="uil uil-gift"></i>'
-x = 30
-var dayToday = mm * x + dd
-/*
-Jan : 0
-Feb : 1
-Mar : 2
-April : 3
-May :  4
-June : 5
-July : 6
-August : 7
-Sept : 8
-Oct : 9
-Nov : 10
-Dec : 11
-*/
-var imageLink = 'https://avatars.dicebear.com/api/personas/male/'
-var persons = [
-  {
-    "name": "Nabeel",
-    "dateOfBirth": "Nov 14",
-    "image": imageLink + 'Nabeel.svg',
-    "over": 10 * x + 14 == dayToday ? bday : notOver,
-    "day": 10 * x + 14,
-  },
-  {
-    "name": "Nawaf",
-    "dateOfBirth": "Sept 20",
-    "image": imageLink + "Nawaf.svg",
-    "over": 8 * x + 20 == dayToday ? bday : notOver,
-    "day": 8 * x + 20,
-  },
-  {
-    "name": "Shaheef",
-    "dateOfBirth": "May 19",
-    "image": imageLink + "Shaheef.svg",
-    "over": 4 * x + 19 == dayToday ? bday : notOver,
-    "day": 4 * x + 19,
-  },
-  {
-    "name": "Saurav",
-    "dateOfBirth": "Nov 02",
-    "image": imageLink + "saurav.svg",
-    "over": 10 * x + 2 == dayToday ? bday : notOver,
-    "day": 10 * x + 2,
-  },
-  {
-    "name": "Arjun",
-    "dateOfBirth": "Jun 16",
-    "image": imageLink + "Arjun.svg",
-    "over": 5 * x + 16 == dayToday ? bday : notOver,
-    "day": 5 * x + 16,
+var over = '<i class="uil uil-check"></i>';
+var notOver = '<i class="uil uil-arrow-up"></i>';
+var bday = '<i class="uil uil-gift"></i>';
+var dayToday = setDay(mm, dd);
 
+const month = {
+  Jan: 0,
+  Feb: 1,
+  Mar: 2,
+  April: 3,
+  May: 4,
+  June: 5,
+  July: 6,
+  August: 7,
+  Sept: 8,
+  Oct: 9,
+  Nov: 10,
+  Dec: 11,
+};
+const data = [
+  {
+    name: "Nabeel",
+    dob: { mm: month.Nov, mmstr: "Nov", dd: 14 },
   },
   {
-    "name": "Alan",
-    "dateOfBirth": "Jan 03",
-    "image": imageLink + "alan.svg",
-    "over": 0 * x + 3 == dayToday ? bday : notOver,
-    "day": 0 * x + 3,
+    name: "Razeena",
+    dob: { mm: month.Dec, mmstr: "Dec", dd: 1 },
   },
   {
-    "name": "Melbin",
-    "dateOfBirth": "Dec 26",
-    "image": imageLink + "Melbin.svg",
-    "over": 11 * x + 26 == dayToday ? bday : notOver,
-    "day": 11 * x + 26,
+    name: "Anjun",
+    dob: { mm: month.July, mmstr: "July", dd: 1 },
   },
   {
-    "name": "Sandesh",
-    "dateOfBirth": "Jan 17",
-    "image": imageLink + "sandesh.svg",
-    "over": 0 * x + 17 == dayToday ? bday : notOver,
-    "day": 0 * x + 17,
+    name: "Nawaf",
+    dob: { mm: month.Sept, mmstr: "Sept", dd: 20 },
   },
   {
-    "name": "Ujwal",
-    "dateOfBirth": "Oct 24",
-    "image": imageLink + "Ujwal.svg",
-    "over": 09 * x + 24 == dayToday ? bday : notOver,
-    "day": 09 * x + 24,
+    name: "Shaheef",
+    dob: { mm: month.May, mmstr: "May", dd: 19 },
   },
   {
-    "name": "Arun",
-    "dateOfBirth": "Oct 31",
-    "image": imageLink + "arun.svg",
-    "over": 09 * x + 31 == dayToday ? bday : notOver,
-    "day": 09 * x + 31,
+    name: "Saurav",
+    dob: { mm: month.Nov, mmstr: "Nov", dd: 02 },
   },
   {
-    "name": "Shameer",
-    "dateOfBirth": "Feb 10",
-    "image": imageLink + "Shameer.svg",
-    "over": 01 * x + 10 == dayToday ? bday : notOver,
-    "day": 01 * x + 10,
+    name: "Arjun",
+    dob: { mm: month.June, mmstr: "Jun", dd: 16 },
   },
   {
-    "name": "Shahabas",
-    "dateOfBirth": "Jul 03",
-    "image": imageLink + "shahbas.svg",
-    "over": 06 * x + 3 == dayToday ? bday : notOver,
-    "day": 06 * x + 3,
+    name: "Alan",
+    dob: { mm: month.Jan, mmstr: "Jan", dd: 03 },
   },
   {
-    "name": "Minhaj",
-    "dateOfBirth": "April 15",
-    "image": imageLink + "minhaj.svg",
-    "over": 03 * x + 15 == dayToday ? bday : notOver,
-    "day": 03 * x + 15,
+    name: "Melbin",
+    dob: { mm: month.Dec, mmstr: "Dec", dd: 26 },
   },
-]
+  {
+    name: "Sandesh",
+    dob: { mm: month.Jan, mmstr: "Jan", dd: 17 },
+  },
+  {
+    name: "Ujwal",
+    dob: { mm: month.Oct, mmstr: "Oct", dd: 24 },
+  },
+  {
+    name: "Arun",
+    dob: { mm: month.Oct, mmstr: "Oct", dd: 31 },
+  },
+  {
+    name: "Shameer",
+    dob: { mm: month.Feb, mmstr: "Feb", dd: 10 },
+  },
+  {
+    name: "Shahabas",
+    dob: { mm: month.July, mmstr: "Jul", dd: 03 },
+  },
+  {
+    name: "Minhaj",
+    dob: { mm: month.April, mmstr: "April", dd: 15 },
+  },
+];
+
+var imageLink = "https://avatars.dicebear.com/api/personas/";
+var persons = [];
+
+for (let i = 0; i < data.length; i++) {
+  persons.push({
+    name: data[i].name,
+    dateOfBirth: `${data[i].dob.mmstr} ${data[i].dob.dd}`,
+    image: imageLink + `${data[i].name}.svg`,
+    over: isDay(setDay(data[i].dob.mm, data[i].dob.dd)),
+    day: setDay(data[i].dob.mm, data[i].dob.dd),
+  });
+}
 
 persons.sort((a, b) => {
   if (!b.day < dayToday && a.day < dayToday) return 1;
@@ -125,12 +105,18 @@ persons.sort((a, b) => {
 });
 
 if (persons[0].day > dayToday) {
-  persons[0].over = '<i class="uil uil-arrow-left"></i>'
+  persons[0].over = '<i class="uil uil-arrow-left"></i>';
 }
 // persons[0].over ='<i class="uil uil-arrow-left"></i>'
 function check(a, today) {
-  var notOver = '<i class="uil uil-arrow-up"></i>'
-  var bday = '<i class="uil uil-gift"></i>'
-  if (a == today) return bday
-  else return notOver
+  var notOver = '<i class="uil uil-arrow-up"></i>';
+  var bday = '<i class="uil uil-gift"></i>';
+  if (a == today) return bday;
+  else return notOver;
+}
+function setDay(m, d) {
+  return m * 30 + d;
+}
+function isDay(x) {
+  return x == dayToday ? bday : notOver;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -48,4 +48,3 @@ const displayCharacters = (characters) => {
 };
 
 loadCharacters();
-
